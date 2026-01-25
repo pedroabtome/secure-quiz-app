96136e4982d503b856440e426f8015049ae932ce
pedroabtome <pedro.afonsotome@gmail.com>
2026-01-25 23:31:28 +0000

    chore: bootstrap spring boot project

---
+++ src/main/java/com/pedroabtome/secure_quiz_app/SecureQuizAppApplication.java
@@ -0,0 +1,13 @@
+package com.pedroabtome.secure_quiz_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureQuizAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureQuizAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedroabtome/secure_quiz_app/SecureQuizAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedroabtome.secure_quiz_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureQuizAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
